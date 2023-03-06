a94082b409b5713388f64d74e1ae02742813f9b7
MiguelRArt <angel4pride@gmail.com>
2023-03-06 11:55:05 -0500

    Recursos Adicionales

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const homeR = `
     <div id="sexi">
         <div id="homeContent" class="homeContent container">
@@ -45,8 +44,55 @@ const contactR = `
 <div class="sexi container" id="sexi">Hola3</div>
 `
 
-const hireUsR = `
-<div class="sexi container" id="sexi">Hola4</div>
+const profileR = `
+<div class="sexi container" id="sexi">
+<div id="profileContent" class="profileContent container">
+        <div class="leftProfile container">
+            <div class="profileInfo container">
+                <img src="src/img/profile-photo.jpg">
+                <h3>Nickname</h3>
+                <h4>Library Colaborator</h4>    
+            </div>
+           
+
+            <div class="container profileOptions">
+                <div class="firstOpt">
+                    <i class="fa-solid fa-book"></i>
+                    <a href="#">Books</a>
+                </div>
+                <div class=" container secondOpt">
+                    <i class="fa-solid fa-clock"></i>
+                    <a href="#">Vigency</a>
+                </div>
+                <div class="container thirdOpt">
+                    <i class="fa-solid fa-check"></i>
+                    <a href="#">Recommendations</a>
+                </div>
+                <button href="mailto:50profe@correo.com" class="contactUs">Contact Us</button>
+            </div>
+        </div>
+
+        <div class="rightProfile container">
+            <h2>Books</h2>
+            <div class="contentBooks container">
+                <div class="books container">
+                    <h3>El cuco de cristal</h3>
+
+                    <img src="src/img/ElCucoDeCristal.gif">
+
+                    <p>¿ESTÁS PREPARADO PARA ARRIESGAR TU CORAZÓN? Vuelve Javier Castillo MAGNÉTICA - SALVAJE - MONUMENTAL - TREPIDANTE El regreso más esperado MÁS DE 1.700.000 EJEMPLARES VENDIDOS Un trasplante de urgencia Un donante lleno de secretos¿Qué esconden los latidos de tu corazón</p>
+                </div>
+                <div class="books container">
+                    <h3>Hijos de la fábula</h3>
+
+                    <img src="src/img//HijosDeLaFabula.gif">
+                    
+                    <p>Dos jóvenes exaltados, Asier y Joseba, se marchan en 2011 al sur de Francia con la intención de convertirse en militantes de ETA. Esperan instrucciones en una granja de pollos, acogidos por una pareja francesa con la que apenas se entienden</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 `
 
 function render(documentHtml, innerHtml){
@@ -61,10 +107,9 @@ function change(documentHtml, contenedor){
 }
 
 render(document.getElementById("right"), homeR);
-/*change(document.getElementById("home"),homeR);
-change(document.getElementById("works"),worksR);
+change(document.getElementById("home"),homeR);
+// change(document.getElementById("works"),worksR);
 change(document.getElementById("about"),aboutR);
 change(document.getElementById("contact"),contactR);
-change(document.getElementById("hireUs"),hireUsR);
+change(document.getElementById("profile"),profileR);
 
-*/
